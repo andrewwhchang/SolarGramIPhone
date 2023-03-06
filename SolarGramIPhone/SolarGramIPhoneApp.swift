07ca3dbd064aee4aa4df213de9f14d176e60694e
andrewwhchang <andrewwhchang1@gmail.com>
2023-03-05 21:24:51 -0500

    Initial Commit

---
+++ SolarGramIPhone/SolarGramIPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramIPhoneApp.swift
+//  SolarGramIPhone
+//
+//  Created by Andrew Chang on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramIPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
