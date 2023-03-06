d50243a4cc2529e23a11bb8e9311591b83cd76ef
andrewwhchang <andrewwhchang1@gmail.com>
2023-03-05 22:11:35 -0500

    Fix Nav3

--- SolarGramIPhone/SolarGramIPhoneApp.swift
+++ SolarGramIPhone/SolarGramIPhoneApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct SolarGramIPhoneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
             NavigationBarView()
         }
     }
--- SolarGramIPhone/VViews/ContentView.swift
+++ SolarGramIPhone/VViews/ContentView.swift
@@ -19,7 +19,6 @@ struct ContentView: View {
         .ignoresSafeArea(edges: .leading)
         .ignoresSafeArea(edges: .trailing)
 //        .padding()
-        //        NavigationBarView()
         
     }
 }
